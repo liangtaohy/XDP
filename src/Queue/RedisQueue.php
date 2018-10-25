507158cb31b71169c589a2ea584165252f595316
Liang Tao <liangtaohy@163.com>
2018-10-25 17:52:52 +0800

    阿里支付

--- src/Queue/RedisQueue.php
+++ src/Queue/RedisQueue.php
@@ -8,8 +8,43 @@
 
 namespace Xdp\Queue;
 
+use Xdp\Contract\Queue\Queue as QueueInterface;
 
-class RedisQueue extends Queue
+class RedisQueue extends Queue implements QueueInterface
 {
+    /**
+     * @var \Xdp\Redis\RedisManager
+     */
+    protected $redis;
+    protected $default;
 
+    public function __construct($redis, $default = 'default')
+    {
+        $this->redis = $redis;
+        $this->default = $default;
+    }
+
+    public function push($queue, string $job, $data = null)
+    {
+        return $this->connection()->rPush($queue, $this->createPayload($job, $data));
+    }
+
+    public function later($queue, $job, $data = null, $delay = 0)
+    {
+    }
+
+    public function size($queue)
+    {
+    }
+
+    protected function connection()
+    {
+        return $this->redis->connection($this->connection_name ?? $this->default);
+    }
+
+    protected function createPayload($job, $data)
+    {
+        $payload = '';
+        return $payload;
+    }
 }
